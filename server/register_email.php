b0f8a55825f6856c974782b71e31961809435748
alex-heritier <alex.heritier@gmail.com>
2020-08-14 22:36:39 -0700

    updated privacy policy

--- server/register_email.php
+++ server/register_email.php
@@ -6,8 +6,4 @@ $message = $_POST['email'] .  " registered for the newsletter.";
 
 mail($to ,$subject ,$message);
 
-// error_log(print_r($GLOBALS, true));
-//
-// echo json_encode($message);
-
 echo true;
