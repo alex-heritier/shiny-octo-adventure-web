fa069c14eb9d41707ca42b7c221e9d36197333fd
alex-heritier <alex.heritier@gmail.com>
2020-08-15 16:05:46 -0700

    improved validation for email input form

--- js/index.js
+++ js/index.js
@@ -1,30 +1,51 @@
+function showRegisterStatus(message, isSuccess) {
+  const resultStatus = document.querySelector('.register-email .result');
+  resultStatus.textContent = message;
+  resultStatus.style = `color: ${isSuccess ? "green" : "red"}`;
+}
+
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 window.onload = function(e) {
   const registerButton = document.querySelector('#register-btn');
 
   registerButton.onclick = function() {
     const emailInput = document.querySelector('#register-input');
-    const params = `email=${emailInput.value}`;
+    const rawEmail = emailInput.value;
+
+    // Check for blank email
+    if (!rawEmail || rawEmail == "") {
+      showRegisterStatus("Value is required", false);
+      return;
+    }
+
+    // Check for valid email format
+    if (!validateEmail(rawEmail)) {
+      showRegisterStatus("Invalid email", false);
+      return;
+    }
 
     fetch("server/register_email.php", {
       method: 'POST',
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: params,
+      body: `email=${rawEmail}`,
       mode: 'no-cors',
     })
     .then(async function(resp) {
       const result = await resp.json();
       console.log(result);
 
-      const resultStatus = document.querySelector('.register-email .result');
       if (result == 1) {
-        resultStatus.textContent = "Great, you'll receive an email soon!";
-        resultStatus.style = "color: green";
+        showRegisterStatus("Great, you'll receive an email soon!", true);
       } else {
-        resultStatus.textContent = "Failed to register";
-        resultStatus.style = "color: red";
+        showRegisterStatus("Failed to register", false);
       }
-    });
+    })
+    .catch((error) => showRegisterStatus("An error occured", false));
   };
 }
