bb627c190296ac6b78c2d56b040f989ef0610220
alex-heritier <alex.heritier@gmail.com>
2020-08-17 18:13:20 -0700

    add newline after writing to emails.txt

--- server/register_email.php
+++ server/register_email.php
@@ -5,7 +5,7 @@ $subject = "Intro Chinese - Newsletter signup";
 $message = $_POST['email'] .  " registered for the newsletter.";
 
 // Append email to email list
-$res = file_put_contents('emails.txt', $_POST['email'], FILE_APPEND | LOCK_EX);
+$res = file_put_contents('emails.txt', $_POST['email']."\n", FILE_APPEND | LOCK_EX);
 
 // Send myself an email
 mail($to ,$subject ,$message);
