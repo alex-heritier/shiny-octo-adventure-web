497e3d162d7eb33e73f645ffd173bc37f8031185
alex-heritier <alex.heritier@gmail.com>
2020-08-17 18:02:29 -0700

    email signup writes to a text file

--- server/register_email.php
+++ server/register_email.php
@@ -4,6 +4,10 @@ $to = "alex.heritier@gmail.com";
 $subject = "Intro Chinese - Newsletter signup";
 $message = $_POST['email'] .  " registered for the newsletter.";
 
+// Append email to email list
+$res = file_put_contents('emails.txt', $_POST['email'], FILE_APPEND | LOCK_EX);
+
+// Send myself an email
 mail($to ,$subject ,$message);
 
 echo true;
